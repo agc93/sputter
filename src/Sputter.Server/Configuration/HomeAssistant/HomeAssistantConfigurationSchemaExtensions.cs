2cf62f723d565f3a877e58d6be1c0b013f6bab27
Alistair Chapman <alistair@agchapman.com>
2024-02-28 14:16:47 +1000

    Tweak HA config loading (again);

--- src/Sputter.Server/Configuration/HomeAssistant/HomeAssistantConfigurationSchemaExtensions.cs
+++ src/Sputter.Server/Configuration/HomeAssistant/HomeAssistantConfigurationSchemaExtensions.cs
@@ -5,9 +5,15 @@ namespace Microsoft.Extensions.Configuration;
 public static class HomeAssistantConfigurationSchemaExtensions {
     public static IConfigurationBuilder AddHomeAssistantAddOnConfiguration
     (this IConfigurationBuilder builder, string? filePath = null) {
-        var source = string.IsNullOrWhiteSpace(filePath)
-            ? new HomeAssistantConfigurationSource { Path = "/data/options.json", Optional = true, ReloadOnChange = true }
-            : new HomeAssistantConfigurationSource { Path = filePath, Optional = true, ReloadOnChange = true };
+        var source = new HomeAssistantConfigurationSource { 
+            Path = filePath ?? "/data/options.json", 
+            Optional = true, 
+            ReloadOnChange = true,
+            OnLoadException = ex => {
+                Console.WriteLine(ex.ToString());
+            }, 
+            ReloadDelay = 1000
+        };
         return builder.Add(source);
     }
 }
